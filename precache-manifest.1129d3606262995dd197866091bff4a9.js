2f7370d9f762dbdbc0394405a02dc0f80b4c557e
K <Kjy_@live.cn>
2020-02-28 16:41:31 +0800

    init commit

---
+++ precache-manifest.1129d3606262995dd197866091bff4a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9fc5d20d8f3f024dc31d30a137d49d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6bc28a9714377cde240",
+    "url": "/static/css/main.5238747f.chunk.css"
+  },
+  {
+    "revision": "70e4449e55d086f9b280",
+    "url": "/static/js/2.c90d1e31.chunk.js"
+  },
+  {
+    "revision": "1345f00e0306e41524b745a6af348b82",
+    "url": "/static/js/2.c90d1e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6bc28a9714377cde240",
+    "url": "/static/js/main.32ee265e.chunk.js"
+  },
+  {
+    "revision": "44f004e9deeb2ae89ccad9fbe844ea7f",
+    "url": "/static/js/main.32ee265e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99f373036fb86e2cc52",
+    "url": "/static/js/runtime-main.097bf332.js"
+  }
+]);
